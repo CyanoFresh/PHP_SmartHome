041279ef24756bc0f21653c214237e6ae50c6980
CyanoFresh <cyanofresh@gmail.com>
2016-05-25 16:35:52 +0300

    Init

---
+++ environments/dev/config/main-local.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    'bootstrap' => ['debug', 'gii'],
+    'components' => [
+        'mailer' => [
+            'useFileTransport' => true,
+        ],
+        'request' => [
+            'cookieValidationKey' => '',
+        ],
+    ],
+    'modules' => [
+        'debug' => [
+            'class' => 'yii\debug\Module',
+        ],
+        'gii' => [
+            'class' => 'yii\gii\Module',
+            'generators' => [
+                'crud' => [
+                    'class' => 'yii\gii\generators\crud\Generator',
+                    'templates' => [
+                        'myCrud' => '@app/giiTemplates/crud/my',
+                    ]
+                ]
+            ],
+        ],
+    ],
+];
