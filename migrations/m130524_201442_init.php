bc61aadc3896e32f5f97759fc1a70c5fe8f55a50
CyanoFresh <cyanofresh@gmail.com>
2016-06-04 20:46:30 +0300

    Migration fixes

--- migrations/m130524_201442_init.php
+++ migrations/m130524_201442_init.php
@@ -7,7 +7,6 @@ class m130524_201442_init extends Migration
 {
     public function safeUp()
     {
-        // Create table
         $tableOptions = null;
 
         if ($this->db->driverName === 'mysql') {
@@ -39,15 +38,16 @@ class m130524_201442_init extends Migration
             'type' => $this->smallInteger()->notNull(),
             'pin' => $this->smallInteger()->notNull(),
             'name' => $this->string()->notNull(),
+            'updateInterval' => $this->float()->notNull()->defaultValue(0),
             'title' => $this->string()->notNull(),
             'icon' => $this->string()->notNull(),
         ], $tableOptions);
 
         $this->createTable('{{%log}}', [
             'id' => $this->primaryKey(),
+            'type' => $this->smallInteger()->notNull(),
             'user_id' => $this->integer()->notNull(),
             'item_id' => $this->integer()->notNull(),
-            'type' => $this->smallInteger()->notNull(),
             'value' => $this->smallInteger()->notNull(),
             'date' => $this->integer()->notNull(),
         ], $tableOptions);
